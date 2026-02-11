6eefd3c9951c32e751b3a2b2027a26f05068127b
Carol <carolfdomingues3@gmail.com>
2026-02-11 15:55:50 -0300

    versão inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Bem-vindo</h1>
+
+        <p>Digite suas credenciais para acessar o sistema</p>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+
+        <button>Entrar</button>
+      </main>
+    </>
+  )
+}
+
